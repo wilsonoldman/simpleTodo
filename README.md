0f66f8aed532e58914b6133c6d4e6c123388bf1b
wowilson <vote4wowilson@gmail.com>
2020-02-27 17:27:54 +0900

    add react-native-storybook-loader and config to package.json

---
+++ README.md
@@ -0,0 +1,10 @@
+# how to use storybook
+add *.stories.js files into ./components or ./containers(the directories can be set in package.json ```config.react-native-storybook-loader```)
+
+```yarn rnstl```
+
+then
+
+```expo start```
+
+The tab screen which is made of StorybookUIRoot will appear in the right of the header.
---
+++ storybook/storyLoader.js
@@ -0,0 +1,17 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+	require('../components/Task.stories');
+}
+
+const stories = [
+	'../components/Task.stories'
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
